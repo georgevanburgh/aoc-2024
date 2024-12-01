fe199066d6fdffdbb1cbd88e8f48f62df9089883
George Vanburgh <1670176+georgevanburgh@users.noreply.github.com>
2024-12-01 12:22:54 +0000

    Day 01

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Parsing
+var input = File.ReadLines("day01/input.txt");
+
+var parsed = input.Select(x => x.Split(' ').Select(int.Parse));
+
+var left = parsed.Select(x => x.First()).Order().ToArray();
+var right = parsed.Select(x => x.Last()).Order().ToArray();
+
+// Part 1
+var result1 = (int) left.Zip(right).Sum(line => Math.Abs(line.First - line.Second));
+Console.WriteLine(result1);
+
+// Part 2
+var counts = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+var result2 = left.Sum(x => x * counts.GetValueOrDefault(x, 0));
+Console.WriteLine(result2);
